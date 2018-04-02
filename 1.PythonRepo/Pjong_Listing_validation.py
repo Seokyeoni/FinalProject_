5f0c5fb2f6903c1b25d6d654c8fb80badb6ce756
meco-p <zkahalf@gmail.com>
2018-04-02 17:31:55 +0900

    encoding issue 1. change column name 2.

--- 1.PythonRepo/Pjong_Listing_validation.py
+++ 1.PythonRepo/Pjong_Listing_validation.py
@@ -6,8 +6,8 @@ import datetime
 #DB에 저장된 정보를 CSV로 하나 유지(모든 파일의 통합) 미리 생성
 #csv는 미리 존재해야함
 
-path_csv = "C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/csv_data/"
-path_info = "C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/"
+path_csv = "C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/csv_data/"
+path_info = "C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/"
 csv_list = os.listdir(path_csv)
 info_list = os.listdir(path_info)
 
@@ -16,17 +16,17 @@ def check_symbol_info():
     if 'symbol_info_update.csv' not in check_exist_info:
         symbol_info = pd.DataFrame()
         for file in csv_list:
-            symbol_new = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/csv_data/"+ file)  
+            symbol_new = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/csv_data/"+ file)  
             symbol_info=symbol_info.append(symbol_new)
-        symbol_info.to_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/symbol_info.csv", index=False, sep=",", encoding='‘utf-8’')
+        symbol_info.to_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/symbol_info.csv", index=False, sep=",", encoding='‘utf-8’')
         
 def listing_validation():
     check_symbol_info()
-    symbol_info = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/symbol_info.csv")
+    symbol_info = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/symbol_info.csv")
     symbol_info["listing"]='x'
     info_vals = symbol_info['symbol'].values
     for file in csv_list:
-        symbol_new = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/csv_data/"+file)
+        symbol_new = pd.read_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/csv_data/"+file)
         vals = symbol_new['symbol'].values
         for symbol in info_vals:
             if symbol in vals:
@@ -40,7 +40,7 @@ def listing_validation():
     update_time = datetime.date.today().isoformat()
     symbol_info["updatetime"]=update_time
     symbol_info.name = "a"
-    symbol_info.to_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.TestData_Pjong/symbol_info_update.csv", index=False, sep=",", encoding='‘utf-8’')
+    symbol_info.to_csv("C:/0.bigdata/4.web/Triple_Core/0.DataRepo/0.StockData/symbol_info.csv", index=False, sep=",", encoding='‘utf-8’')
 
     #불러와서 저장
 listing_validation()
